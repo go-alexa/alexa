97ad044061b2f8bd8f93a5d651bd45f1c7c6a6cd
Greg Cordover <ghc@ixchi.me>
2017-02-17 13:34:02 -0600

    Update example to include using slots.

--- server/server_test.go
+++ server/server_test.go
@@ -23,6 +23,13 @@ func ExampleServer() {
 			func(ev *parser.Event) (*response.Response, error) {
 				return response.New().
 					AddSpeech("Hello, world!"), nil
+			}).
+		Add("HelloName",
+			func(ev *parser.Event) (*response.Response, error) {
+				name := ev.Request.Intent.Slots["Name"].Value
+
+				return response.New().
+					AddSpeech("Hello, " + name), nil
 			})
 
 	Run(ev)
