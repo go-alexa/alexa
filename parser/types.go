ec70744b94778043177b4313d55a43f8ba8deada
Danilo Topalovic <danilo.topalovic@nerdsee.com>
2017-05-12 16:33:29 +0200

    added the context branch
    added the context branch to the request tree

--- parser/types.go
+++ parser/types.go
@@ -29,19 +29,24 @@ type Application struct {
 	ID string `json:"applicationId"`
 }
 
+type Permissions struct {
+	ConsentToken string `json:"consentToken,omitempty"`
+}
+
 // User is information about the user, including access token if one has been
 // set through linking an account.
 type User struct {
-	ID          string `json:"userId"`
-	AccessToken string `json:"accessToken"`
+	ID          string      `json:"userId"`
+	AccessToken string      `json:"accessToken"`
+	Permissions Permissions `json:"permissions,omitempty"`
 }
 
 // Request is information about the request, including the intent and data.
 type Request struct {
 	ID        string `json:"requestId"`
 	Type      string `json:"type"`
-	Timestamp *Time  `json:"timestamp"`
 	Locale    string `json:"locale,omitempty"`
+	Timestamp *Time  `json:"timestamp"`
 	Intent    Intent `json:"intent,omitempty"`
 	Reason    string `json:"reason,omitempty"`
 }
@@ -51,17 +56,17 @@ type Time time.Time
 
 // MarshalJSON allows for encoding the timestamp in the correct format.
 func (t Time) MarshalJSON() ([]byte, error) {
-	return []byte(time.Time(t).Format(time.RFC3339Nano)), nil
+	return []byte(time.Time(t).Format(time.RFC3339)), nil
 }
 
 // UnmarshalJSON allows for decoding the time in the correct format.
 func (t *Time) UnmarshalJSON(b []byte) error {
-	time, err := time.Parse("\""+time.RFC3339Nano+"\"", string(b))
+	parsedTime, err := time.Parse("\""+time.RFC3339+"\"", string(b))
 	if err != nil {
 		return err
 	}
 
-	*t = Time(time)
+	*t = Time(parsedTime)
 
 	return nil
 }
@@ -73,8 +78,9 @@ func (t Time) ToTime() time.Time {
 
 // Intent is information about the intent, including its name and slots.
 type Intent struct {
-	Name  string          `json:"name"`
-	Slots map[string]Slot `json:"slots"`
+	Name  				string 			`json:"name"`
+	Slots 				map[string]Slot `json:"slots,omitempty"`
+	ConfirmationStatus  string			`json:"confirmationStatus,omitempty"`
 }
 
 // Slot is the data for an intent.
@@ -82,3 +88,29 @@ type Slot struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
 }
+
+type AudioPlayer struct {
+	PlayerActivity string `json:"playerActivity,omitempty"`
+}
+
+type SupportedInterfaces struct {
+	AudioPlayer AudioPlayer `json:"AudioPlayer,omitempty"`
+}
+
+type Device struct {
+	ID string						`json:"deviceId,omitempty"`
+	Interfaces SupportedInterfaces  `json:"supportedInterfaces,omitempty"`
+}
+
+type System struct {
+	Application Application `json:"application,omitempty"`
+	User        User        `json:"user,omitempty"`
+	Device		Device      `json:"device,omitempty"`
+	ApiEndpoint string		`json:"apiEndpoint,omitempty"`
+}
+
+type Context struct {
+	AudioPlayer AudioPlayer `json:"AudioPlayer,omitempty"`
+	System      System      `json:"System,omitempty"`
+}
+
